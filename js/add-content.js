6c4b4a9e426a25c4551038911c1d8d38de540407
Amber Falbo <amberannfalbo@gmail.com>
2020-04-15 15:06:49 -0700

    create a construction site with js and html

---
+++ js/add-content.js
@@ -0,0 +1,17 @@
+// add JS content below
+
+var today = new Date();
+var hourNow = today.getHours();
+var greeting;
+
+if (hourNow > 18) {
+    greeting = 'Good eventing!';
+} else if (hourNow > 12) {
+    greeting = 'Good afternoon!';
+} else if (hourNow > 0) {
+    gretting = 'Good morning!';
+} else {
+    greeting = 'Welcome!';
+}
+
+document.write('<h3>' + greeting + '</h3>');
